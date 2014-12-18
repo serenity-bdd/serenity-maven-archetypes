d1cf4e15168cb3a290240cf13da8617eb78113b6
John Ferguson Smart <john.smart@wakaleo.com>
2014-12-18 13:22:09 +1100

    Refactored directory names

---
+++ serenity-cucumber-archetype/src/main/resources/archetype-resources/src/test/java/cucumber/DefinitionSteps.java
@@ -0,0 +1,33 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.cucumber;
+
+import net.thucydides.core.annotations.Steps;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import ${package}.steps.EndUserSteps;
+
+public class DefinitionSteps {
+
+    @Steps
+    EndUserSteps endUser;
+
+    @Given("the user is on the Wikionary home page")
+    public void givenTheUserIsOnTheWikionaryHomePage() {
+        endUser.is_the_home_page();
+    }
+
+    @When("the user looks up the definition of the word '(.*)'")
+    public void whenTheUserLooksUpTheDefinitionOf(String word) {
+        endUser.looks_for(word);
+    }
+
+    @Then("they should see the definition '(.*)'")
+    public void thenTheyShouldSeeADefinitionContainingTheWords(String definition) {
+        endUser.should_see_definition(definition);
+    }
+
+}
---
+++ serenity-cucumber-archetype/src/main/resources/archetype-resources/src/test/java/cucumber/DefinitionTestSuite.java
@@ -0,0 +1,12 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.cucumber;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
+public class DefinitionTestSuite {}
