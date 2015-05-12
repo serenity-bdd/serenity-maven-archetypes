4d20baac4ce3845a4deff03f7e41400f939000ef
Kishen Simbhoedatpanday <ksimbhoedatpanday@xebia.com>
2015-05-12 12:03:52 +0200

    Import cleanup

--- serenity-cucumber-archetype/src/main/resources/archetype-resources/src/main/java/pages/DictionaryPage.java
+++ serenity-cucumber-archetype/src/main/resources/archetype-resources/src/main/java/pages/DictionaryPage.java
@@ -6,12 +6,10 @@ package ${package}.pages;
 import ch.lambdaj.function.convert.Converter;
 import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import net.thucydides.core.pages.WebElementFacade;
+import net.serenitybdd.core.pages.WebElementFacade;
 
-import net.thucydides.core.annotations.findby.FindBy;
+import net.serenitybdd.core.annotations.findby.FindBy;
 
 import net.thucydides.core.pages.PageObject;
 
--- serenity-cucumber-archetype/src/main/resources/archetype-resources/src/main/java/steps/EndUserSteps.java
+++ serenity-cucumber-archetype/src/main/resources/archetype-resources/src/main/java/steps/EndUserSteps.java
@@ -5,10 +5,8 @@ package ${package}.steps;
 
 import ${package}.pages.DictionaryPage;
 import net.thucydides.core.annotations.Step;
-import net.thucydides.core.pages.Pages;
 import net.thucydides.core.steps.ScenarioSteps;
 
-import static ch.lambdaj.Lambda.join;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
