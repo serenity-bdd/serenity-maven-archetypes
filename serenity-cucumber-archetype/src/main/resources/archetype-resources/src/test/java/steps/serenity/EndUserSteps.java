49d3a0ec8336aa608a3ab43bc427069867f3972e
John Smart <john.smart@wakaleo.com>
2016-10-03 11:25:40 +0100

    Updated test scenarios

--- serenity-cucumber-archetype/src/main/resources/archetype-resources/src/test/java/steps/serenity/EndUserSteps.java
+++ serenity-cucumber-archetype/src/main/resources/archetype-resources/src/test/java/steps/serenity/EndUserSteps.java
@@ -11,7 +11,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
 
-public class EndUserSteps extends ScenarioSteps {
+public class EndUserSteps {
 
     DictionaryPage dictionaryPage;
 
